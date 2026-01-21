6263f71129f53ef41424ff31a9778308b8b0d11e
unknown <68010876@kmitl.ac.th>
2026-01-21 15:46:42 +0700

    p03

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,15 @@
-// Write your code below
+const FizzBuzz = Number(process.argv[2]);
+if (FizzBuzz === undefined){
+    console.log("Invalid input")
+}
+    for (let i = 1; i <= FizzBuzz; i++) {
+        if (i % 15 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
