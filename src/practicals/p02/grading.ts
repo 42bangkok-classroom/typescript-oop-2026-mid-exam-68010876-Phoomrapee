573dc15748b5e7a1b685af5f605e121207761a37
unknown <68010876@kmitl.ac.th>
2026-01-21 15:11:53 +0700

    p02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,19 @@
-// Write your code below
+const input = Number.process.argv[2];
+if (input === undefined || input === ""){
+    console.log("Invalid input")
+}
+if (input >= 80){
+    console.log("Grade is A")
+}
+if (input >= 70){
+        console.log("Grade is B")
+    }
+if (input >= 60){
+        console.log("Grade is C")
+    }
+if (input >= 50){
+        console.log("Grade is D")
+    }
+    else {
+        console.log("Grade is F")
+    }
